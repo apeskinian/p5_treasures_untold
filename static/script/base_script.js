626cb57d68e8ca8f30116a820f116427263858a8
Matthew Aitken <matt@itken.net>
2025-01-21 14:16:26 +0000

    Update mobile nav links

--- static/script/base_script.js
+++ static/script/base_script.js
@@ -1,2 +1,8 @@
+// enable tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// stop the realm collapse menu from triggering the
+document.querySelector('#by-realm-menu-mobile a').addEventListener('click', function (event) {
+    event.stopPropagation();
+  });
