e9dd9d1e539e78fb6789d401bc4ad460c2197679
Matthew Aitken <matt@itken.net>
2025-01-27 14:37:51 +0000

    Update context processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
@@ -10,12 +12,25 @@ def basket_contents(request):
     product_count = 0
     discount = settings.DISCOUNT
     delivery = settings.DELIVERY
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if discount > 0:
-        discounted_total = Decimal(total-(total * discount/100))
-        grand_total = discounted_total + delivery
+        discounted_total = (
+            Decimal(total) - (Decimal(total) * Decimal(discount) / 100)
+        )
+        grand_total = discounted_total + Decimal(delivery)
     else:
-        grand_total = total + delivery
+        grand_total = Decimal(total) + Decimal(delivery)
 
     context = {
         'basket_items': basket_items,
