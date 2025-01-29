bfb17c89576e25f3c52f753d52085bfd81d3a8b9
Matthew Aitken <matt@itken.net>
2025-01-29 16:57:00 +0000

    Create checkout template, install crispy forms

--- treasures_untold/settings.py
+++ treasures_untold/settings.py
@@ -57,7 +57,13 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -87,6 +93,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'basket.contexts.basket_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
--- treasures_untold/urls.py
+++ treasures_untold/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
+    path('checkout/', include('checkout.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
