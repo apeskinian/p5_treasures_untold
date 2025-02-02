de9b0e75e46e3ca90d4b2b56e07127e99150d7ec
Matthew Aitken <matt@itken.net>
2025-02-02 21:03:53 +0000

    Update stripe_elements.js
    Previous versions had consistent issues, this now works following stripe documentation

--- checkout/static/checkout/script/stripe_elements.js
+++ checkout/static/checkout/script/stripe_elements.js
@@ -1,22 +1,27 @@
+// Core logic/payment flow for this comes from here:
+// https://docs.stripe.com/payments/accept-a-payment?platform=web&ui=elements
+
 var stripePublicKey = JSON.parse(document.getElementById('id_stripe_public_key').textContent);
 var clientSecret = JSON.parse(document.getElementById('id_client_secret').textContent);
 
 var stripe = Stripe(stripePublicKey);
 
-const appearance = { /* appearance */ };
-const options = { 
-    layout: 'accordion', /* options */ };
-const elements = stripe.elements({ clientSecret, appearance });
-const paymentElement = elements.create('payment', options);
-paymentElement.mount('#card-element');
+const options = {
+  clientSecret,
+  appearance: {/*...*/},
+};
 
-// var elements = stripe.elements();
-// var paymentElement = elements.create('card');
-// paymentElement.mount('#card-element')
+// Set up Stripe.js and Elements to use in checkout form, passing the client secret
+const elements = stripe.elements(options);
+
+// Create and mount the Payment Element
+const paymentElementOptions = { layout: 'accordion'};
+const paymentElement = elements.create('payment', paymentElementOptions);
+paymentElement.mount('#payment-element');
 
 // handling realtime validation errors
 paymentElement.addEventListener('change', function(event) {
-    var errorDiv = $('.card-errors');
+    var errorDiv = $('#payment-errors');
     if (event.error) {
         var html = event.error.message;
         $(errorDiv).html(html);
@@ -25,105 +30,42 @@ paymentElement.addEventListener('change', function(event) {
     }
 })
 
+const form = document.getElementById('payment-form');
 
-// // handling payment submit: code from STRIPE
-// https://docs.stripe.com/payments/quickstart
-
-const form = document.querySelector("#payment-form");
-form.addEventListener("submit", handleSubmit);
-
-async function handleSubmit(e) {
-  e.preventDefault();
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
   setLoading(true);
 
-  const { error } = await stripe.confirmPayment({
+  const {error} = await stripe.confirmPayment({
+    //`Elements` instance that was used to create the Payment Element
     elements,
     redirect: 'if_required',
-    confirmParams: {
-      // Make sure to change this to your payment completion page
-      // return_url: "http://127.0.0.1:8000/checkout/",
-    },
+    confirmParams: {},
   });
-  
-  // This point will only be reached if there is an immediate error when
-  // confirming the payment. Otherwise, your customer will be redirected to
-  // your `return_url`. For some payment methods like iDEAL, your customer will
-  // be redirected to an intermediate site first to authorize the payment, then
-  // redirected to the `return_url`.
 
   if (error) {
-    if (error.type === "card_error" || error.type === "validation_error") {
-      showMessage(error.message);
-    } else {
-      showMessage("An unexpected error occurred.");
-    }
+    // show any errors to the user
+    var errorDiv = $('#payment-errors');
+    var html = error.message;
+        $(errorDiv).html(html);
+    setLoading(false);
+  } else {
+    // submit the form
+    form.submit()
   }
+});
 
-  form.submit()
-  setLoading(false);
-
-  
-}
-
-// ------- UI helpers -------
-function showMessage(messageText) {
-  const messageContainer = document.querySelector("#payment-message");
-
-  messageContainer.classList.remove("hidden");
-  messageContainer.textContent = messageText;
-
-  setTimeout(function () {
-    messageContainer.classList.add("hidden");
-    messageContainer.textContent = "";
-  }, 4000);
-}
-
-// Show a spinner on payment submission
+// disable submit and adjust buttons and show a spinner to user
 function setLoading(isLoading) {
   if (isLoading) {
-    // Disable the button and show a spinner
-    document.querySelector("#submit").disabled = true;
-    document.querySelector("#adjust").disabled = true;
+    document.querySelector("#submit-button").disabled = true;
+    document.querySelector("#adjust-button").disabled = true;
     document.querySelector("#spinner").classList.remove("hidden");
     document.querySelector("#button-text").classList.add("hidden");
   } else {
-    document.querySelector("#submit").disabled = false;
-    document.querySelector("#adjust").disabled = false;
+    document.querySelector("#submit-button").disabled = false;
+    document.querySelector("#adjust-button").disabled = false;
     document.querySelector("#spinner").classList.add("hidden");
     document.querySelector("#button-text").classList.remove("hidden");
   }
 }
-
-
-
-
-
-// handle form submit
-// var form = document.getElementById('payment-form');
-
-// form.addEventListener('submit', function(ev) {
-//     ev.preventDefault();
-//     paymentElement.update({ 'disabled': true});
-//     $('#submit').attr('disabled', true);
-//     stripe.confirmCardPayment(clientSecret, {
-//         payment_method: {
-//             card: paymentElement,
-//         }
-//     }).then(function(result) {
-//         if (result.error) {
-//             var errorDiv = document.getElementById('card-errors');
-//             var html = `
-//                 <span class="icon" role="alert">
-//                 <i class="fas fa-times"></i>
-//                 </span>
-//                 <span>${result.error.message}</span>`;
-//             $(errorDiv).html(html);
-//             paymentElement.update({ 'disabled': false});
-//             $('#submit-button').attr('disabled', false);
-//         } else {
-//             if (result.paymentIntent.status === 'succeeded') {
-//                 console.log('WORKED')
-//             }
-//         }
-//     });
-// });
--- checkout/views.py
+++ checkout/views.py
@@ -9,7 +9,6 @@ import stripe
 def checkout(request):
     if request.method == 'POST':
         messages.success(request, "Your payment was successful!")
-        return redirect('products')
 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
