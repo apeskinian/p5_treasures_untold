9993a5b9b6d75d9f3e58c43047b4ef07b868a57c
Matthew Aitken <matt@itken.net>
2025-02-05 09:40:47 +0000

    Comment out order saves, try and find ghost order

--- checkout/views.py
+++ checkout/views.py
@@ -37,82 +37,83 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-        basket = request.session.get('basket', {})
+    # if request.method == 'POST':
+    #     basket = request.session.get('basket', {})
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'street_address_1': request.POST['street_address_1'],
-            'street_address_2': request.POST['street_address_2'],
-            'town_city': request.POST['town_city'],
-            'county': request.POST['county'],
-            'postcode': request.POST['postcode'],
-            'country': request.POST['country'],
-            'phone_number': request.POST['phone_number']
-        }
+    #     form_data = {
+    #         'full_name': request.POST['full_name'],
+    #         'street_address_1': request.POST['street_address_1'],
+    #         'street_address_2': request.POST['street_address_2'],
+    #         'town_city': request.POST['town_city'],
+    #         'county': request.POST['county'],
+    #         'postcode': request.POST['postcode'],
+    #         'country': request.POST['country'],
+    #         'phone_number': request.POST['phone_number']
+    #     }
 
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_basket = json.dumps(basket)
-            order.save()
-            for item_id, quantity in basket.items():
-                try:
-                    product = Product.objects.get(pk=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=quantity,
-                    )
-                    order_line_item.save()
-                except Product.DoesNotExist:
-                    messages.error(request, (
-                        'One of the products in the basket was lost fom our'
-                        ' database. Please contact us for assistance'
-                    ))
-                    order.delete()
-                    return redirect(reverse('view_basket'))
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(
-                reverse('checkout_success', args=[order.order_number])
-            )
-        else:
-            messages.error(request, 'There was an error with your form. \
-                           Please double check your information.')
-    else:
-        basket = request.session.get('basket', {})
-        if not basket:
-            messages.error(
-                request, "There's nothing in your basket at the moment"
-            )
-            return redirect(reverse('products'))
-
-        current_basket = basket_contents(request)
-        total = current_basket['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
+    #     order_form = OrderForm(form_data)
+    #     if order_form.is_valid():
+    #         order = order_form.save(commit=False)
+    #         pid = request.POST.get('client_secret').split('_secret')[0]
+    #         order.stripe_pid = pid
+    #         order.original_basket = json.dumps(basket)
+    #         print('SAVING ORDER FROM VIEW...')
+    #         order.save()
+    #         for item_id, quantity in basket.items():
+    #             try:
+    #                 product = Product.objects.get(pk=item_id)
+    #                 order_line_item = OrderLineItem(
+    #                     order=order,
+    #                     product=product,
+    #                     quantity=quantity,
+    #                 )
+    #                 order_line_item.save()
+    #             except Product.DoesNotExist:
+    #                 messages.error(request, (
+    #                     'One of the products in the basket was lost fom our'
+    #                     ' database. Please contact us for assistance'
+    #                 ))
+    #                 order.delete()
+    #                 return redirect(reverse('view_basket'))
+    #         request.session['save_info'] = 'save-info' in request.POST
+    #         return redirect(
+    #             reverse('checkout_success', args=[order.order_number])
+    #         )
+    #     else:
+    #         messages.error(request, 'There was an error with your form. \
+    #                        Please double check your information.')
+    # else:
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(
+            request, "There's nothing in your basket at the moment"
         )
+        return redirect(reverse('products'))
 
-    try:
-        profile = UserProfile.objects.get(user=request.user)
-        order_form = OrderForm(initial={
-            'full_name': profile.default_full_name,
-            'phone_number': profile.default_phone_number,
-            'street_address_1': profile.default_street_address_1,
-            'street_address_2': profile.default_street_address_2,
-            'town_city': profile.default_town_city,
-            'county': profile.default_county,
-            'postcode': profile.default_postcode,
-            'country': profile.default_country,
-        })
-    except UserProfile.DoesNotExist:
-        order_form = OrderForm()
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
+    # try:
+    #     profile = UserProfile.objects.get(user=request.user)
+    #     order_form = OrderForm(initial={
+    #         'full_name': profile.default_full_name,
+    #         'phone_number': profile.default_phone_number,
+    #         'street_address_1': profile.default_street_address_1,
+    #         'street_address_2': profile.default_street_address_2,
+    #         'town_city': profile.default_town_city,
+    #         'county': profile.default_county,
+    #         'postcode': profile.default_postcode,
+    #         'country': profile.default_country,
+    #     })
+    # except UserProfile.DoesNotExist:
+
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
@@ -127,27 +128,27 @@ def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
-    save_info = request.session.get('save_info')
+    # save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    profile = UserProfile.objects.get(user=request.user)
-    order.user_profile = profile
-    order.save()
+    # profile = UserProfile.objects.get(user=request.user)
+    # order.user_profile = profile
+    # order.save()
 
-    if save_info:
-        profile_data = {
-            'default_full_name': order.full_name,
-            'default_phone_number': order.phone_number,
-            'default_street_address_1': order.street_address_1,
-            'default_street_address_2': order.street_address_2,
-            'default_town_city': order.town_city,
-            'default_postcode': order.postcode,
-            'default_county': order.county,
-            'default_country': order.country,
-        }
-        user_profile_form = UserProfileForm(profile_data, instance=profile)
-        if user_profile_form.is_valid:
-            user_profile_form.save()
+    # if save_info:
+    #     profile_data = {
+    #         'default_full_name': order.full_name,
+    #         'default_phone_number': order.phone_number,
+    #         'default_street_address_1': order.street_address_1,
+    #         'default_street_address_2': order.street_address_2,
+    #         'default_town_city': order.town_city,
+    #         'default_postcode': order.postcode,
+    #         'default_county': order.county,
+    #         'default_country': order.country,
+    #     }
+    #     user_profile_form = UserProfileForm(profile_data, instance=profile)
+    #     if user_profile_form.is_valid:
+    #         user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
                      Your order number is {order_number}. \
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,6 +5,7 @@ import stripe
 
 from django.http import HttpResponse
 
+# from profiles.models import UserProfile
 from products.models import Product
 from .models import Order, OrderLineItem
 
@@ -48,63 +49,82 @@ class StripeWH_Handler:
             if value == '':
                 shipping_details.address[field] = None
 
-        order_exists = False
-        attempt = 1
-        while attempt <= 5:
-            try:
-                order = Order.objects.get(
-                    full_name__iexact=shipping_details.name,
-                    phone_number__iexact=shipping_details.phone,
-                    street_address_1__iexact=shipping_details.address.line1,
-                    street_address_2__iexact=shipping_details.address.line2,
-                    town_city__iexact=shipping_details.address.city,
-                    county__iexact=shipping_details.address.state,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    country__iexact=shipping_details.address.country,
-                    grand_total=grand_total,
-                    original_basket=basket,
-                    stripe_pid=pid,
-                )
-                order_exists = True
-                break
-            except Order.DoesNotExist:
-                attempt += 1
-                time.sleep(1)
-        if order_exists:
-            return HttpResponse(
-                content='Order verified in database', status=200
-            )
-        else:
-            order = None
-            try:
-                order = Order.objects.create(
-                    full_name=shipping_details.name,
-                    phone_number=shipping_details.phone,
-                    street_address_1=shipping_details.address.line1,
-                    street_address_2=shipping_details.address.line2,
-                    town_city=shipping_details.address.city,
-                    county=shipping_details.address.state,
-                    postcode=shipping_details.address.postal_code,
-                    country=shipping_details.address.country,
-                    grand_total=grand_total,
-                    original_basket=basket,
-                    stripe_pid=pid,
-                )
-                for item_id, quantity in json.loads(basket).items():
-                    product = Product.objects.get(pk=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=quantity,
-                    )
-                    order_line_item.save()
-            except Exception as e:
-                if order:
-                    order.delete()
-                return HttpResponse(
-                    content=f': {event['type']} | ERRROR: {e}',
-                    status=500
-                )
+        # update user profile information if save_info was checked
+        # profile = None
+        # username = intent.metadata.username
+        # if username != 'AnonymousUser':
+        #     profile = UserProfile.objects.get(user__username=username)
+        #     if save_info:
+        #         profile.default_full_name = shipping_details.name,
+        #         profile.default_phone_number = shipping_details.phone,
+        #         profile.default_street_address_1 = shipping_details.address.line1,
+        #         profile.default_street_address_2 = shipping_details.address.line2,
+        #         profile.default_town_city = shipping_details.address.city,
+        #         profile.default_county = shipping_details.address.state,
+        #         profile.default_postcode = shipping_details.address.postal_code,
+        #         profile.default_country = shipping_details.address.country,
+        #         profile.save()
+
+        # order_exists = False
+        # attempt = 1
+        # while attempt <= 5:
+        #     try:
+        #         order = Order.objects.get(
+        #             full_name__iexact=shipping_details.name,
+        #             phone_number__iexact=shipping_details.phone,
+        #             street_address_1__iexact=shipping_details.address.line1,
+        #             street_address_2__iexact=shipping_details.address.line2,
+        #             town_city__iexact=shipping_details.address.city,
+        #             county__iexact=shipping_details.address.state,
+        #             postcode__iexact=shipping_details.address.postal_code,
+        #             country__iexact=shipping_details.address.country,
+        #             grand_total=grand_total,
+        #             original_basket=basket,
+        #             stripe_pid=pid,
+        #         )
+        #         order_exists = True
+        #         break
+        #     except Order.DoesNotExist:
+        #         attempt += 1
+        #         time.sleep(1)
+        # if order_exists:
+        #     return HttpResponse(
+        #         content='Order verified in database', status=200
+        #     )
+        # else:
+        #     order = None
+        #     try:
+        #         print('CREATING WEBHOOK ORDER...')
+        #         order = Order.objects.create(
+        #             # full_name=shipping_details.name,
+        #             # user_profile=profile,
+        #             full_name='REAL WEBHOOK ORDER',
+        #             phone_number=shipping_details.phone,
+        #             street_address_1=shipping_details.address.line1,
+        #             street_address_2=shipping_details.address.line2,
+        #             town_city=shipping_details.address.city,
+        #             county=shipping_details.address.state,
+        #             postcode=shipping_details.address.postal_code,
+        #             country=shipping_details.address.country,
+        #             grand_total=grand_total,
+        #             original_basket=basket,
+        #             stripe_pid=pid,
+        #         )
+        #         for item_id, quantity in json.loads(basket).items():
+        #             product = Product.objects.get(pk=item_id)
+        #             order_line_item = OrderLineItem(
+        #                 order=order,
+        #                 product=product,
+        #                 quantity=quantity,
+        #             )
+        #             order_line_item.save()
+        #     except Exception as e:
+        #         if order:
+        #             order.delete()
+        #         return HttpResponse(
+        #             content=f': {event['type']} | ERRROR: {e}',
+        #             status=500
+        #         )
         return HttpResponse(
             content=f'TU Webhook received: {event['type']} |'
             'SUCCESS order created in webhook', status=200
