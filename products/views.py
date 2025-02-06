bf73817f7642b130948396c15740d9ae0e418718
Matthew Aitken <matt@itken.net>
2025-02-06 20:59:54 +0000

    Edit product functionality added

--- products/views.py
+++ products/views.py
@@ -145,3 +145,33 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """
+    edits a product in the shop
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'{product.name} successfully updated.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request,
+                'Error in updating product, please ensure form is valid'
+            )
+    else:
+        form = ProductForm(instance=product)
+
+    messages.info(request, f'Editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product
+    }
+
+    return render(request, template, context)
