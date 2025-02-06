098d9c519fdcb5affcfce583fdd22c604de0cb8f
Matthew Aitken <matt@itken.net>
2025-02-06 11:55:01 +0000

    Add email field to order model

--- checkout/models.py
+++ checkout/models.py
@@ -20,13 +20,14 @@ class Order(models.Model):
         related_name='orders'
     )
     full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
     street_address_1 = models.CharField(max_length=80, null=False, blank=False)
     street_address_2 = models.CharField(max_length=80, null=True, blank=True)
     town_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
     date = models.DateField(auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0
