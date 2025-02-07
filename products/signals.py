503159c1be1d0edb1bb0598f2d30be5bbaa09565
Matthew Aitken <matt@itken.net>
2025-02-07 12:41:32 +0000

    Update cloudinary delete signal

--- products/signals.py
+++ products/signals.py
@@ -7,6 +7,6 @@ from .models import Product
 @receiver(post_delete, sender=Product)
 def delete_image_from_cloudinary(sender, instance, **kwargs):
     # making sure the image is not a placeholder and deleting image
-    if instance.image and instance.image != 'placeholder':
-        public_id = str(instance.image)
+    if instance.image and instance.image.public_id != 'placeholder':
+        public_id = instance.image.public_id
         destroy(public_id)
