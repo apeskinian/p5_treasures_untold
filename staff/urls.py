41418505697a942e989888c1ca5a08e4e93ed57d
Matthew Aitken <matt@itken.net>
2025-02-11 12:01:11 +0000

    Refactor delete view into manage_faq

--- staff/urls.py
+++ staff/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('manage_faq/', views.manage_faq, name='manage_faq'),
     path('manage_faq/<int:faq_id>/', views.manage_faq, name='manage_faq'),
-    # path('manage_faq/<str:delete>/<int:faq_id>/', views.manage_faq, name='manage_faq'),
-    path('delete_faq/<int:faq_id>/', views.delete_faq, name='delete_faq'),
+    path(
+        'manage_faq/<str:delete>/<int:faq_id>/',
+        views.manage_faq, name='manage_faq'
+    ),
+    # path('delete_faq/<int:faq_id>/', views.delete_faq, name='delete_faq'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -28,10 +28,10 @@ def dashboard(request):
 @staff_member_required
 def manage_faq(request, delete=None, faq_id=None):
     """
-    add or edit a faq
+    add, edit or delete a faq
     """
     faqs = Faqs.objects.all()
-    mode = 'Update' if faq_id else 'Add'
+    mode = 'Delete' if delete and faq_id else 'Update' if faq_id else 'Add'
     return_url = f"{reverse('dashboard')}?tab=FAQ"
     faq = None
 
@@ -39,19 +39,28 @@ def manage_faq(request, delete=None, faq_id=None):
         faq = get_object_or_404(Faqs, pk=faq_id)
 
     if request.method == 'POST':
-        form = FaqsForm(request.POST, instance=faq)
-        if form.is_valid():
-            form.save()
-            messages.success(
-                request,
-                'FAQ updated' if faq_id else 'FAQ Added'
-            )
-            return redirect(return_url)
+        if delete:
+            try:
+                faq.delete()
+                messages.success(request, 'FAQ deleted')
+                return redirect(return_url)
+            except Exception as e:
+                messages.error(request, f'Error deleting FAQ: {e}')
+                return redirect(return_url)
         else:
-            messages.error(
-                request,
-                'Failed to update FAQ' if faq_id else 'Failed to add FAQ'
-            )
+            form = FaqsForm(request.POST, instance=faq)
+            if form.is_valid():
+                form.save()
+                messages.success(
+                    request,
+                    'FAQ updated' if faq_id else 'FAQ Added'
+                )
+                return redirect(return_url)
+            else:
+                messages.error(
+                    request,
+                    'Failed to update FAQ' if faq_id else 'Failed to add FAQ'
+                )
     else:
         form = FaqsForm(instance=faq)
 
@@ -59,38 +68,13 @@ def manage_faq(request, delete=None, faq_id=None):
     context = {
         'active_tab': 'FAQ',
         'faqs': faqs,
-        'form': form,
         'mode': mode,
-        'return_url': return_url
+        'return_url': return_url,
     }
-    print(context)
-
-    return render(request, template, context)
-
-
-@staff_member_required
-def delete_faq(request, faq_id):
-    """
-    deletes a faq
-    """
-    faqs = Faqs.objects.all()
-    faq = get_object_or_404(Faqs, pk=faq_id)
-    return_url = f"{reverse('dashboard')}?tab=FAQ"
-    if request.method == 'POST':
-        try:
-            faq.delete()
-            messages.success(request, 'FAQ deleted')
-            return redirect(return_url)
-        except Exception as e:
-            messages.error(request, f'Error deleting FAQ: {e}')
-            return redirect(return_url)
+    if mode == 'Delete':
+        context['to_delete'] = faq
     else:
-        template = 'staff/dashboard.html'
-        context = {
-            'active_tab': 'FAQ',
-            'faq_to_delete': faq,
-            'faqs': faqs,
-            'return_url': return_url
-        }
+        context['form'] = form
+    print(context)
 
     return render(request, template, context)
