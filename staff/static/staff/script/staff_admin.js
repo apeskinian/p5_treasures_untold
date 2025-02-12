565aa6132b0b42ab6939a1cbe5618eb5b0630602
Matthew Aitken <matt@itken.net>
2025-02-12 09:05:01 +0000

    Add comments to staff_admin.js

--- staff/static/staff/script/staff_admin.js
+++ staff/static/staff/script/staff_admin.js
@@ -1,4 +1,4 @@
-// STAFF DASHBOARD SRCIPT //
+// STAFF DASHBOARD SCRIPT //
 
 // Showing modal if there is an add, update or delete request.
 window.onload = () => {
@@ -9,40 +9,42 @@ window.onload = () => {
     }
 };
 
-
+// Display chosen image filename when selecting image for product
 $('#new-image').change(function() {
     var file = $('#new-image')[0].files[0];
     $('#filename').text(`${file.name}`);
 });
 
+// Display new image next to current image when updating product
 const imageInput = document.querySelector('input[name="image"]');
 const previewImage = document.getElementById('previewImage');
 const newImagePreview = document.getElementById('new-image-preview')
 
 if (imageInput) {
-    imageInput.addEventListener("change", function (event) {
+    imageInput.addEventListener('change', function (event) {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = function (e) {
                 previewImage.src = e.target.result;
-                newImagePreview.style.display = "block";
+                newImagePreview.style.display = 'block';
             };
             reader.readAsDataURL(file);
         }
     });
 }
 
-const idTopic = document.getElementById("id_topic")
+// Show new topic field in FAQ form if new topic is selected
+const idTopic = document.getElementById('id_topic')
 
 if (idTopic) {
-    idTopic.addEventListener("change", function() {
-        let newTopicField = document.getElementById("new-topic-field");
-        if (this.value === "new") {
-            newTopicField.style.display = "block";
+    idTopic.addEventListener('change', function() {
+        let newTopicField = document.getElementById('new-topic-field');
+        if (this.value === 'new') {
+            newTopicField.style.display = 'block';
         } else {
-            newTopicField.style.display = "none";
-            document.getElementById("id_new_topic").value = "";
+            newTopicField.style.display = 'none';
+            document.getElementById('id_new_topic').value = '';
         }
     });
 }
@@ -76,4 +78,3 @@ if (dashboardModalConfirmButton) {
         buttonElement.querySelector('.spinner-border').classList.remove('d-none');
     })
 }
-// DASHBOARD BUTTON SPINNERS //
