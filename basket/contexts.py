2eefc10e364c2502e955e89b4d0af199c47ed63c
Matthew Aitken <matt@itken.net>
2025-02-16 13:11:20 +0000

    Enable cave-of-wonders reward

--- basket/contexts.py
+++ basket/contexts.py
@@ -14,10 +14,13 @@ def basket_contents(request):
     delivery = settings.DELIVERY
     basket = request.session.get('basket', {})
     rewards = request.session.get('rewards', [])
+    print(rewards)
 
     for index, (item_id, quantity) in enumerate(basket.items()):
         product = get_object_or_404(Product, pk=item_id)
         if index < 3 and 'magic-lamp' in rewards:
+            product.price = 0
+        if product.realm.name == 'Agrabah' and 'cave-of-wonders' in rewards:
             product.price = 0
         total += quantity * product.price
         product_count += quantity
