c24f2f0397aa9f15df74b4a9716194e0e9fd7aa7
Matthew Aitken <matt@itken.net>
2025-02-26 18:47:34 +0000

    Renable command and session expiry

--- basket/management/commands/clear_abandoned_sessions.py
+++ basket/management/commands/clear_abandoned_sessions.py
@@ -10,66 +10,66 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         print('CURRENTLY DISABLED BUT WHY IS THIS BEING CALLED???')
-        # basket_counter = 0
-        # item_counter = 0
-        # now = datetime.now()
-        # expiry_time = now - timedelta(minutes=10)
+        basket_counter = 0
+        item_counter = 0
+        now = datetime.now()
+        expiry_time = now - timedelta(minutes=10)
 
-        # all_sessions = Session.objects.all()
+        all_sessions = Session.objects.all()
 
-        # for session in all_sessions:
-        #     session_data = session.get_decoded()
-        #     last_modified_str = session_data.get('modified')
+        for session in all_sessions:
+            session_data = session.get_decoded()
+            last_modified_str = session_data.get('modified')
 
-        #     if not last_modified_str:
-        #         print(
-        #             f'Skipping session {session.session_key}: '
-        #             'No modified time found.'
-        #         )
-        #         continue
+            if not last_modified_str:
+                print(
+                    f'Skipping session {session.session_key}: '
+                    'No modified time found.'
+                )
+                continue
 
-        #     try:
-        #         last_modified = datetime.strptime(
-        #             last_modified_str, '%d/%m/%Y, %H:%M:%S'
-        #         )
-        #     except ValueError as e:
-        #         print(
-        #             f'Skipping session {session.session_key}: '
-        #             f'Invalid modified format - {e}'
-        #         )
-        #         continue
+            try:
+                last_modified = datetime.strptime(
+                    last_modified_str, '%d/%m/%Y, %H:%M:%S'
+                )
+            except ValueError as e:
+                print(
+                    f'Skipping session {session.session_key}: '
+                    f'Invalid modified format - {e}'
+                )
+                continue
 
-        #     if last_modified < expiry_time:
-        #         if 'basket' in session_data:
-        #             basket = session_data['basket']
+            if last_modified < expiry_time:
+                if 'basket' in session_data:
+                    basket = session_data['basket']
 
-        #             for item_id, quantity in basket.items():
-        #                 try:
-        #                     product = get_object_or_404(Product, pk=item_id)
-        #                     updated_stock = product.stock
-        #                     updated_stock += quantity
-        #                     if updated_stock < 0:
-        #                         raise ValueError('Stock cannot be negative.')
-        #                     elif product.unique_stock and updated_stock > 1:
-        #                         raise ValueError(
-        #                             'Stock cannot be more than 1 '
-        #                             'for unique items'
-        #                         )
-        #                     else:
-        #                         item_counter += quantity
-        #                         product.stock += quantity
-        #                         product.save()
-        #                 except ValueError as e:
-        #                     print(
-        #                         f"Error updating stock for {product.id}: {e}"
-        #                     )
+                    for item_id, quantity in basket.items():
+                        try:
+                            product = get_object_or_404(Product, pk=item_id)
+                            updated_stock = product.stock
+                            updated_stock += quantity
+                            if updated_stock < 0:
+                                raise ValueError('Stock cannot be negative.')
+                            elif product.unique_stock and updated_stock > 1:
+                                raise ValueError(
+                                    'Stock cannot be more than 1 '
+                                    'for unique items'
+                                )
+                            else:
+                                item_counter += quantity
+                                product.stock += quantity
+                                product.save()
+                        except ValueError as e:
+                            print(
+                                f"Error updating stock for {product.id}: {e}"
+                            )
 
-        #             basket_counter += 1
+                    basket_counter += 1
 
-        #         session.delete()
+                session.delete()
 
-        # self.stdout.write(
-        #     self.style.SUCCESS(
-        #         f'Recovered {item_counter} items from {basket_counter} baskets'  # noqa
-        #     )
-        # )
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Recovered {item_counter} items from {basket_counter} baskets'  # noqa
+            )
+        )
