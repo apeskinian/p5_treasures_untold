9d9d58e8aa98748579e36c210f564dd059fac725
Matthew Aitken <matt@itken.net>
2025-03-03 14:59:38 +0000

    Adjust sorting of realms

--- products/contexts.py
+++ products/contexts.py
@@ -1,9 +1,10 @@
 from .models import Realm
+from django.db.models.functions import Lower
 
 
 def get_realms(request):
     """
     returns a queryset of all current realms in db sorted alphabetically
     """
-    all_realms = Realm.objects.all().order_by('name')
+    all_realms = Realm.objects.all().order_by(Lower('name'))
     return {'all_realms': all_realms}
