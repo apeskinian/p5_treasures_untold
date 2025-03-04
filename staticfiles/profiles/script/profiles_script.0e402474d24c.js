b07cb020a6f6a1bc002a2b7f105dae007ea29177
Matthew Aitken <matt@itken.net>
2025-03-04 16:39:03 +0000

    Add 404 and 500 pages

---
+++ staticfiles/profiles/script/profiles_script.0e402474d24c.js
@@ -0,0 +1,8 @@
+// Showing modal if there is an order history request.
+window.onload = () => {
+    const orderHistoryModalElement = document.getElementById('order-history-modal');
+    if (orderHistoryModalElement) {
+        const orderHistoryModal = new bootstrap.Modal(orderHistoryModalElement);
+        orderHistoryModal.show();
+    }
+};
