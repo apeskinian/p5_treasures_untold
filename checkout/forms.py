c708ae200a5051360a6e5003c5024799bcebf7a2
Matthew Aitken <matt@itken.net>
2025-03-05 14:13:28 +0000

    refactor: checkout/forms.py
    add docstrings, comments and confirm PEP8 format

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from .models import Order
 
 
@@ -18,10 +19,10 @@ class OrderForm(forms.ModelForm):
         )
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated labels and set
-        autofocus  on first field
-        """
+        '''
+        Initialize the order form with custom placeholders, remove
+        auto-generated labels, and set autofocus on the 'full_name' field.
+        '''
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
