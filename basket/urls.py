5aa09cc66d35517fde158136e097935bb56ce4b9
Matthew Aitken <matt@itken.net>
2025-03-05 12:06:16 +0000

    refactor: basket/urls.py
    add docstrings, comments and confirm PEP8 format

--- basket/urls.py
+++ basket/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # View the contents of the basket.
     path('', views.view_basket, name='view_basket'),
+
+    # Add an item to the basket using item_id as an argument.
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+
+    # Adjust an item in the basket using item_id as an argument.
     path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
+
+    # Remove an item from the basket using item_id as an argument.
     path(
         'remove/<item_id>/',
         views.remove_from_basket,
