d8cccbe7d3a9c474a844665fd39510a39bba3be5
Matthew Aitken <matt@itken.net>
2025-03-05 09:16:02 +0000

    refactor: basket/apps.py
    add docstrings, comments and confirm PEP8 format

--- basket/apps.py
+++ basket/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class BasketConfig(AppConfig):
+    """
+    Configuration for the 'basket' app.
+    This app handles the shopping basket functionality.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'basket'
