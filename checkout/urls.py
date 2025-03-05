6c7e658e08c3d1400bdb94f772d5cb75de3b32fa
Matthew Aitken <matt@itken.net>
2025-03-05 14:44:07 +0000

    refactor: checkout/urls.py
    add docstrings, comments and confirm PEP8 format

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,18 +1,27 @@
 from django.urls import path
+
 from . import views
 from .webhooks import webhook
 
+
 urlpatterns = [
+    # View for completing purchse and processing payment.
     path('', views.checkout, name='checkout'),
+
+    # Confirmation view of transaction.
     path(
         'checkout_success/<order_number>',
         views.checkout_success,
         name='checkout_success'
     ),
+
+    # Adding extra data to payment intent metadata.
     path(
         'cache_checkout_data/',
         views.cache_checkout_data,
         name='cache_checkout_data'
     ),
+
+    # Webhook handler
     path('wh/', webhook, name='webhook')
 ]
