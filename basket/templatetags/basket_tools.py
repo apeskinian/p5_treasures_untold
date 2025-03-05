931e75a7368cb7c70c45fa5f7586ff0781d1bfba
Matthew Aitken <matt@itken.net>
2025-03-05 17:09:21 +0000

    refactor: basket_tools.py
    add docstrings, comments and confirm PEP8 format

--- basket/templatetags/basket_tools.py
+++ basket/templatetags/basket_tools.py
@@ -5,4 +5,14 @@ register = template.Library()
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """
+    Calculates the subtotal of a product based on its price and quantity.
+
+    **Arguments:**
+    - 'price' (Decimal or float): The price of a single unit of the product.
+    - 'quantity' (int): The number of units of the product.
+
+    **Returns:**
+    - Decimal or float: The total cost (price * quantity).
+    """
     return price * quantity
