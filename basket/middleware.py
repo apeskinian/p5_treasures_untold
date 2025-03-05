e432e1053a54aa21da02ff880414c6e74635f865
Matthew Aitken <matt@itken.net>
2025-03-05 12:00:28 +0000

    refactor: basket/middleware.py
    add docstrings, comments and confirm PEP8 format

--- basket/middleware.py
+++ basket/middleware.py
@@ -2,10 +2,22 @@ from datetime import datetime
 
 
 class UpdateSessionMiddleware:
+    """
+    Middleware to update the 'modified' timestamp of the session.
+    Used when checking for abandoned baskets.
+    """
     def __init__(self, get_response):
+        """
+        Initiliazes the middleware.
+        """
         self.get_response = get_response
 
     def __call__(self, request):
+        """
+        Sets the 'modified' timestamp in the session data.
+        **Returns:**
+        - The response from the next middleware or view in the stack.
+        """
         if request.session.session_key:
             request.session['modified'] = (
                 datetime.now().strftime('%d/%m/%Y, %H:%M:%S')
