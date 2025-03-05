90abfce058b6a049915baffa1d6f7131731c4550
Matthew Aitken <matt@itken.net>
2025-03-05 17:24:51 +0000

    refactor: checkout_tools.py
    add docstrings, comments and confirm PEP8 format

--- checkout/templatetags/checkout_tools.py
+++ checkout/templatetags/checkout_tools.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+
 from django import template
 
 register = template.Library()
@@ -6,4 +7,14 @@ register = template.Library()
 
 @register.filter(name='calc_original_total')
 def calc_original_total(total):
+    """
+    This filter calculates the original total price before applying a 20%
+    discount.
+
+    **Args:**
+    - 'total': The total price after the 20% discount.
+
+    **Returns:**
+    - Decimal: The original total price before the discount was applied.
+    """
     return total / Decimal(0.8)
