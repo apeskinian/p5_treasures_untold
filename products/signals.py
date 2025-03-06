79155072c95e13edbd4796c0c42b6a2553ddd921
Matthew Aitken <matt@itken.net>
2025-03-06 10:58:20 +0000

    refactor: products/signals.py
    add docstrings, comments and confirm PEP8 format

--- products/signals.py
+++ products/signals.py
@@ -1,12 +1,22 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+
 from cloudinary.uploader import destroy
+
 from .models import Product
 
 
 @receiver(post_delete, sender=Product)
 def delete_image_from_cloudinary(sender, instance, **kwargs):
-    # making sure the image is not a placeholder and deleting image
+    """
+    Deletes the image from Cloudinary when a product is deleted, unless the
+    image is a placeholder.
+
+    **Arguments:**
+    - 'sender': The model class that sent the signal.
+    - 'instance': The instance of the product that was deleted.
+    - 'kwargs': Additional keyword arguments passed with the signal.
+    """
     if instance.image and instance.image.public_id != 'placeholder':
         public_id = instance.image.public_id
         destroy(public_id)
