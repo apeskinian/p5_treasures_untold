9571846a56381a6ec6d1e5ffca7ef407843232c7
Matthew Aitken <matt@itken.net>
2025-03-06 18:01:18 +0000

    Correct spelling in profiles docstrings

--- profiles/apps.py
+++ profiles/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class ProfilesConfig(AppConfig):
     """
-    Configuration for the `profiles` app.
+    Configuration for the 'profiles' app.
     This app handles the user profiles including viewing order history and
     updating stored details.
     """
