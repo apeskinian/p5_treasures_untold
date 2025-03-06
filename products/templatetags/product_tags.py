41d3e0b96af45497d34d1c8ee600d058b06039fa
Matthew Aitken <matt@itken.net>
2025-03-06 13:13:25 +0000

    refactor: products/product_tags.py
    add docstrings, comments and confirm PEP8 format

--- products/templatetags/product_tags.py
+++ products/templatetags/product_tags.py
@@ -5,4 +5,14 @@ register = template.Library()
 
 @register.filter(name='friendly_name')
 def friendly_name(name):
+    """
+    Converts a string with underscores into a human-readable format
+    by replacing underscores with spaces.
+
+    **Arguments:**
+    - 'name': A string to be formatted.
+
+    **Returns:**
+    - A string where all underscores are replaced with spaces.
+    """
     return name.replace('_', ' ')
