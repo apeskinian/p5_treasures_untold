60a4b50baef58d2d9ec57acb324085aded4921db
Matthew Aitken <matt@itken.net>
2025-03-06 09:33:28 +0000

    refactor: home/views.py
    add docstrings, comments and confirm PEP8 format

--- home/views.py
+++ home/views.py
@@ -1,21 +1,41 @@
+import random
+
+from django.db.models.functions import TruncDate
 from django.shortcuts import render, get_object_or_404
+
 from products.models import Product, Realm
 from profiles.models import UserProfile
-from django.db.models.functions import TruncDate
-import random
 
 
 def index(request):
     """
     A view to return the index page including a queryset for the newest
-    products added to the store and one for a featured realm which
-    is decided each week
+    products added to the store and one for a featured realm which is randomly
+    picked on each page load.
+
+    **Arguments:**
+    - 'request': The HTTP request.
+
+    **Context:**
+    - 'current_user': The current user if logged in.
+    - 'new_products': queryset of :model:`products.Product` filtered to include
+        the newest products added.
+    - 'featured_realm': queryset of :model:`products.Realm` chosen at random.
+    - 'featured_products': queryset of :model:`products.Product` filtered to
+        include products from the featured realm.
+
+    **Template:**
+    - :template:`home/index.html`
+
+    **Returns:**
+    - A render response showing the homepage.
     """
+    # Getting username if logged in.
     current_user = None
-    # getting username if logged in
     if request.user.is_authenticated:
         current_user = get_object_or_404(UserProfile, user=request.user)
-    # getting items for new section
+
+    # Getting items for new section.
     most_recent_dates = (
         Product.objects.annotate(added_date=TruncDate('date_added'))
         .values('added_date')
@@ -33,8 +53,8 @@ def index(request):
     else:
         new_products = Product.objects.none()
 
-    # get a realm to feature and create queryset but pick again if the
-    # realm has no products
+    # Get a realm to feature and create queryset but pick again if the
+    # realm has no products.
     featured_products = Product.objects.none()
     while featured_products.count() < 1:
         realm_count = Realm.objects.count()
@@ -42,7 +62,7 @@ def index(request):
         featured_realm = Realm.objects.all()[random_index]
         featured_products = Product.objects.filter(realm=featured_realm)
 
-    # setting up view parameters
+    # Setting up view parameters.
     template = 'home/index.html'
     context = {
         'current_user': current_user,
