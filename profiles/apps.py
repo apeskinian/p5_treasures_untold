305ded1c57abb3608fb117c61b5ebf50e955dbf2
Matthew Aitken <matt@itken.net>
2025-03-06 13:16:33 +0000

    refactor: profiles/apps.py
    add docstrings, comments and confirm PEP8 format

--- profiles/apps.py
+++ profiles/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
+    """
+    Configuration for the 'profiles' app.
+    This app handles the user profiles including viewing order history and
+    updating stored details.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
