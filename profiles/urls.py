64ae3c1c7f630a5b414c98437a18fd1ec0077570
Matthew Aitken <matt@itken.net>
2025-03-06 13:27:01 +0000

    refactor: profiles/urls.py
    add docstrings, comments and confirm PEP8 format

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+
 from . import views
 
+
 urlpatterns = [
+    # View to show the user profile.
     path('', views.profile, name='profile'),
+
+    # View a previous order.
     path(
         'order_history/<order_number>',
         views.profile,
