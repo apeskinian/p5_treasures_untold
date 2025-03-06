be60383197525a24d8699899437ed1e13259ec1a
Matthew Aitken <matt@itken.net>
2025-03-06 11:01:58 +0000

    refactor: products/urls.py
    add docstrings, comments and confirm PEP8 format

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
+    # View to show all products.
     path('', views.all_products, name='products'),
+
+    # Shows a product in more detail with options to add to the basket.
     path('<int:product_id>/', views.product_detail, name='product_detail'),
+
+    # View to activate or deactivate a reward with the standard arguments.
     path(
         'activate_reward/<str:action>/<str:reward>/',
         views.activate_reward,
         name='activate_reward'),
+
+    # View to activate or deactivate rewards with an extra argument.
     path(
         'activate_reward/<str:action>/<str:reward>/<str:extra>',
         views.activate_reward,
