a426e46565a7465d4d58574eaaddb5dea2108f88
Matthew Aitken <matt@itken.net>
2025-03-06 10:04:38 +0000

    refactor: products/apps.py
    add docstrings, comments and confirm PEP8 format

--- products/apps.py
+++ products/apps.py
@@ -2,8 +2,17 @@ from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
+    """
+    Configuration for the 'products' app.
+    This app handles the products.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
 
     def ready(self):
+        """
+        Import signal handlers to ensure they are registered when the app is
+        ready. This allows the 'products' app to trigger custom signals where
+        necessary.
+        """
         import products.signals  # noqa: F401
