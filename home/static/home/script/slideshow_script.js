f438ef32a2ae8bf65e45dc060ee8318d7be247d6
Matthew Aitken <matt@itken.net>
2025-03-06 09:49:37 +0000

    refactor: slideshow_script.js
    add comments and confirm JSHint format

--- home/static/home/script/slideshow_script.js
+++ home/static/home/script/slideshow_script.js
@@ -1,25 +1,30 @@
+/* jshint esversion: 11 */
+// Slideshow script for homepage.
+
 let newSlides = document.querySelectorAll(".new-slide");
 let featuredSlides = document.querySelectorAll(".featured-slide");
 let newSlidesIndex = 0;
 let featuredSlidesIndex = 0;
 
+// Iterate to next slide and make active.
 function showNextNewSlide() {
     newSlides[newSlidesIndex].classList.remove("active");
     newSlidesIndex = (newSlidesIndex + 1) % newSlides.length;
     newSlides[newSlidesIndex].classList.add("active");
 }
 
+// Iterate to next slide and make active.
 function showNextFeaturedSlide() {
     featuredSlides[featuredSlidesIndex].classList.remove("active");
     featuredSlidesIndex = (featuredSlidesIndex + 1) % featuredSlides.length;
     featuredSlides[featuredSlidesIndex].classList.add("active");
 }
 
-
+// Set initial slides to active.
 newSlides[newSlidesIndex].classList.add("active");
 featuredSlides[featuredSlidesIndex].classList.add("active");
 
-
+// Set times for slide changes.
 setInterval(() => {
     showNextNewSlide();
 }, 6000);
