7c97554f679cbd7c57ed9f64576a47c853445d37
Matthew Aitken <matt@itken.net>
2025-03-06 13:55:13 +0000

    refactor: staff/apps.py
    add docstrings, comments and confirm PEP8 format

--- staff/apps.py
+++ staff/apps.py
@@ -2,5 +2,18 @@ from django.apps import AppConfig
 
 
 class StaffConfig(AppConfig):
+    """
+    Configuration for the 'staff' app.
+
+    This app provides a staff dashboard where authorized users can:
+    - Manage products and realms (add, edit, delete).
+    - Manage FAQs and FAQ topics.
+    - Read and respond to 'Contact Us' messages.
+    - Create and send newsletters.
+    - View and delete previously sent newsletters.
+    - View current subscribers and their status.
+    - Manually remove individual subscribers.
+    - Clear all expired subscribers in one action.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'staff'
