7e6643a02110813eab9a3770fdc767c0b9b30ee5
Matthew Aitken <matt@itken.net>
2025-03-06 11:04:41 +0000

    refactor: products/widgets.py
    add docstrings, comments and confirm PEP8 format

--- products/widgets.py
+++ products/widgets.py
@@ -3,6 +3,18 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
+    """
+    Custom widget for handling file input with a clearable option.
+
+    **Attributes:**
+    - 'clear_checkbox_label': The label for the checkbox to clear file input.
+    - 'initial_text': Text shown for the current file (if any).
+    - 'input_text': Placeholder text for the file input field.
+    - 'template_name': Path to the custom template used for rendering.
+
+    **Template:**
+    - :template:`products/custom_widget_templates/custom_clearable_file_input.html` # noqa: E501
+    """
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
