7e1bddc68cc60f1795d0a8d745ba7c8d61ab31d2
Matthew Aitken <matt@itken.net>
2025-03-06 10:09:17 +0000

    refactor: products/contexts.py
    add docstrings, comments and confirm PEP8 format

--- products/contexts.py
+++ products/contexts.py
@@ -1,10 +1,20 @@
-from .models import Realm
 from django.db.models.functions import Lower
+
+from .models import Realm
 
 
 def get_realms(request):
     """
-    returns a queryset of all current realms in db sorted alphabetically
+    Provides global access to all realms in the app, ordered case-insensitively
+    by name.
+
+    **Arguments:**
+    - `request`: The HTTP request.
+
+    **Returns:**
+    - A dictionary containing:
+        - `'all_realms'`: A queryset of :model:`products.Realm`, ordered
+        case-insensitively by name.
     """
     all_realms = Realm.objects.all().order_by(Lower('name'))
     return {'all_realms': all_realms}
