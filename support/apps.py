4fbe9fa2e32387a108fe7301d5d28ddf71529221
Matthew Aitken <matt@itken.net>
2025-03-07 09:57:27 +0000

    refactor: support/apps.py
    add docstrings, comments and confirm PEP8 format

--- support/apps.py
+++ support/apps.py
@@ -2,5 +2,16 @@ from django.apps import AppConfig
 
 
 class SupportConfig(AppConfig):
+    """
+    Configuration for the 'staff' app.
+
+    This app provides a support pages for users:
+    - A FAQ page.
+    - A contact us page where users can submit a message with their email
+        adress for a reply.
+    - Newletter page where users can find out about the newsletter and enter
+        their email address to sign up.
+    - Privacy policy where users can read the privacy policy.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'support'
