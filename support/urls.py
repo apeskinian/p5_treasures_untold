fd1a25d15459e8e8b0887a11a32651a7c462f801
Matthew Aitken <matt@itken.net>
2025-03-07 10:33:33 +0000

    refactor: support/urls.py
    add docstrings, comments and confirm PEP8 format

--- support/urls.py
+++ support/urls.py
@@ -2,18 +2,34 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('faq/', views.faq, name='faq'),
-    path('contact/', views.contact, name='contact'),
-    path('newsletter/', views.newsletter, name='newsletter'),
-    path('subscribe/', views.subscribe, name='subscribe'),
-    path('privacy/', views.privacy, name='privacy'),
-    path('thankyou/', views.thankyou, name='thankyou'),
-    path('confirm/<int:subscriber_id>/<token>/',
-         views.confirm_subscription,
-         name='confirm_subscription'
-         ),
-    path('unsubscribe/<int:subscriber_id>/<token>/',
-         views.confirm_unsubscription,
-         name='confirm_unsubscription'
-         ),
+     # View to the FAQs page.
+     path('faq/', views.faq, name='faq'),
+
+     # View to the contact us page.
+     path('contact/', views.contact, name='contact'),
+
+     # View to the newsletter page.
+     path('newsletter/', views.newsletter, name='newsletter'),
+
+     # View to the subscribe page.
+     path('subscribe/', views.subscribe, name='subscribe'),
+
+     # View to the privacy policy page.
+     path('privacy/', views.privacy, name='privacy'),
+
+     # View the contact us receipt page.
+     path('thankyou/', views.thankyou, name='thankyou'),
+
+     # View to the subscription confirmation page.
+     path('confirm/<int:subscriber_id>/<token>/',
+          views.confirm_subscription,
+          name='confirm_subscription'
+          ),
+
+     # View to the unsubscribe confirmation which send the user to the home
+     # page with a message..
+     path('unsubscribe/<int:subscriber_id>/<token>/',
+          views.confirm_unsubscription,
+          name='confirm_unsubscription'
+          ),
 ]
