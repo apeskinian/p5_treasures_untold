ff21cca13d3ab3692ff63f4953c91c70c3902cea
Matthew Aitken <matt@itken.net>
2025-03-12 10:30:43 +0000

    Refactor product_limits.js
    Comment and format for jshint

--- static/script/product_limits.js
+++ static/script/product_limits.js
@@ -1,4 +1,5 @@
-// disabling qty modifiers outside of stock ranges
+/* jshint esversion: 11, globalstrict: true, jquery: true */
+// Disabling qty modifiers outside of stock ranges
 function handleEnableDisable(itemId) {
     var currentValue = parseInt($(`#id-qty-${itemId}`).val());
     var maxPurchase = parseInt($(`#id-qty-${itemId}`).attr('max'));
@@ -9,20 +10,20 @@ function handleEnableDisable(itemId) {
     $(`#increment-qty-${itemId}`).prop('disabled', plusDisabled);
 }
 
-// check enable/disable inputs on page load
+// Check enable/disable inputs on page load
 var allQtyInputs = $('.qty-input');
 for (var i = 0; i < allQtyInputs.length; i++) {
     var itemId = $(allQtyInputs[i]).data('item-id');
     handleEnableDisable(itemId);
 }
 
-// enforce limits on built in modifiers
+// Enforce limits on built in modifiers
 $('.qty-input').change(function () {
     var itemId = $(this).data('item-id');
     handleEnableDisable(itemId);
-})
+});
 
-//increment qty
+//Increment qty
 $('.increment-qty').click(function (e) {
     e.preventDefault();
     var closestInput = $(this).closest('.input-group').find('.qty-input')[0];
@@ -32,7 +33,7 @@ $('.increment-qty').click(function (e) {
     handleEnableDisable(itemId);
 });
 
-//decrement qty
+//Decrement qty
 $('.decrement-qty').click(function (e) {
     e.preventDefault();
     var closestInput = $(this).closest('.input-group').find('.qty-input')[0];
@@ -42,11 +43,11 @@ $('.decrement-qty').click(function (e) {
     handleEnableDisable(itemId);
 });
 
-// update form
+// Update form
 $('.update-basket').click(function (e) {
     e.preventDefault();
 
-    // preventing double clicking
+    // Preventing double clicking
     var updateLink = $(this);
     if (updateLink.hasClass('disabled')) return;
     updateLink.addClass('disabled');
@@ -63,16 +64,16 @@ $('.update-basket').click(function (e) {
 
 });
 
-// get csrf token from meta
+// Get csrf token from meta
 function getCsrfToken() {
     return $('meta[name="csrf-token"]').attr('content');
 }
 
-// remove items
+// Remove items
 $('.remove-item').click(function (e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    // prevent double clicking
+    // Prevent double clicking
     var removeLink = $(this);
     if (removeLink.hasClass('disabled')) return;
     removeLink.addClass('disabled');
@@ -90,5 +91,4 @@ $('.remove-item').click(function (e) {
         .fail(function () {
             removeLink.removeClass('disabled');
         });
-})
-
+});
