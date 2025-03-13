c18b93d848494c1035d268d15aa0adfdd84a4e4d
Matthew Aitken <matt@itken.net>
2025-03-13 11:57:33 +0000

    Change order.date to DateTimeField

--- checkout/models.py
+++ checkout/models.py
@@ -62,7 +62,7 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=False, blank=False)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0
     )
