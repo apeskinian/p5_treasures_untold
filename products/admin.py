35ad3e62aff67214a96ed4ac5a449c2b72e82c44
Matthew Aitken <matt@itken.net>
2025-03-14 18:08:18 +0000

    Create admin method to restock items

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Realm, Product
 from django.db.models.functions import Lower
+
+from .models import Realm, Product
 
 
 @admin.register(Realm)
@@ -18,3 +19,18 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ('stock', 'unique_stock',)
     list_filter = ('realm',)
     ordering = (Lower('realm__name'), 'name')
+
+    @admin.action(description='Restock items')
+    def restock(self, request, queryset):
+        """
+        Restocks selected item makes stock level 50 unless the item is unique
+        and then sets it to 1
+        """
+        for product in queryset:
+            if product.unique_stock:
+                product.stock = 1
+            else:
+                product.stock = 50
+            product.save()
+
+    actions = [restock]
