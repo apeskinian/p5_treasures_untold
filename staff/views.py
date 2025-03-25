a38a61edfe95d7e7563b101341fb55bbb7f12888
Matthew Aitken <matt@itken.net>
2025-03-25 13:19:58 +0000

    Change active tab in dashboard - Message

--- staff/views.py
+++ staff/views.py
@@ -227,7 +227,7 @@ def dashboard(request):
 
     # Set current tab to Product if one can't be found.
     if not context.get('active_tab'):
-        context['active_tab'] = active_tab or 'FAQ'
+        context['active_tab'] = active_tab or 'Message'
 
     return render(request, template, context)
 
