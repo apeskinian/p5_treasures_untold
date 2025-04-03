adce01cff99e5d7f96da9085388a2611b05d2011
Matthew Aitken <matt@itken.net>
2025-04-03 10:58:25 +0100

    Fix #82
    actions were looking in request and not the actual queryset of sessions on the db

--- basket/admin.py
+++ basket/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.sessions.models import Session
+from django.contrib.sessions.backends.db import SessionStore
 from django.shortcuts import get_object_or_404
 
 from products.models import Product
@@ -11,31 +12,41 @@ def empty_basket(modeladmin, request, queryset):
     Empty the basket of the selected session and recover any items that are
     currently in the basket.
     """
-    if 'basket' in request.session:
-        basket = request.session.get('basket', {})
+    for session in queryset:
+        session_data = session.get_decoded()
+        if 'basket' in session_data:
+            basket = session_data.get('basket', {})
 
-        for item_id, quantity in basket.items():
-            try:
-                product = get_object_or_404(Product, pk=item_id)
-                updated_stock = product.stock
-                updated_stock += quantity
-                if updated_stock < 0:
-                    raise ValueError('Stock cannot be negative.')
-                elif product.unique_stock and updated_stock > 1:
-                    raise ValueError(
-                        'Stock cannot be more than one for unique items.'
-                    )
-                else:
-                    product.stock += quantity
-                    product.save()
-            except ValueError:
-                pass
+            # Iterate through basket and restock for each item.
+            for item_id, quantity in basket.items():
+                try:
+                    product = get_object_or_404(Product, pk=item_id)
+                    updated_stock = product.stock
+                    updated_stock += quantity
+                    if updated_stock < 0:
+                        raise ValueError('Stock cannot be negative.')
+                    elif product.unique_stock and updated_stock > 1:
+                        raise ValueError(
+                            'Stock cannot be more than one for unique items.'
+                        )
+                    else:
+                        product.stock += quantity
+                        product.save()
+                except ValueError:
+                    pass
 
-        del request.session['basket']
-        modeladmin.message_user(
-            request,
-            'Selected basket(s) have been cleared. Stock has been recovered.'
-        )
+            # Delete the basket from the session and save.
+            del session_data['basket']
+            store = SessionStore(session_key=session.session_key)
+            store.modified = True
+            store.clear()
+            store.update(session_data)
+            store.save()
+
+    modeladmin.message_user(
+        request,
+        'Selected basket(s) have been cleared. Stock has been recovered.'
+    )
 
 
 @admin.action(description='Deactivate rewards')
@@ -43,12 +54,20 @@ def clear_rewards(modeladmin, request, queryset):
     """
     Clears any currently activated rewards in the selected session.
     """
-    if 'rewards' in request.session:
-        del request.session['rewards']
-        modeladmin.message_user(
-            request,
-            'Reward data has been cleared for selected sessions.'
-        )
+    for session in queryset:
+        session_data = session.get_decoded()
+        if 'rewards' in session_data:
+            del session_data['rewards']
+            store = SessionStore(session_key=session.session_key)
+            store.modified = True
+            store.clear()
+            store.update(session_data)
+            store.save()
+
+    modeladmin.message_user(
+        request,
+        'Reward data has been cleared for selected sessions.'
+    )
 
 
 @admin.register(Session)
