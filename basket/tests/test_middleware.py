210b8bac88e1235bda049e9754f2067163dd8c56
Matthew Aitken <matt@itken.net>
2025-04-04 12:09:01 +0100

    Update TESTING.md: Unit test basket/middleware.py

---
+++ basket/tests/test_middleware.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from django.test import TestCase, RequestFactory
+from django.contrib.sessions.backends.db import SessionStore
+
+from ..middleware import UpdateSessionMiddleware
+
+
+class UpdateSessionMiddelwareTest(TestCase):
+    def setUp(self):
+        """Sets up a request and session for testing."""
+        self.middleware = UpdateSessionMiddleware(lambda x: None)
+        self.factory = RequestFactory()
+
+        self.session1 = SessionStore()
+        self.session1.save()
+
+        self.request = self.factory.get('/')
+        self.request.session = self.session1
+
+    def test_modified_session_data(self):
+        """
+        Testing that the middleware updates the session with a 'modified'
+        timestamp.
+        """
+        # Call the middlware.
+        self.middleware(self.request)
+        # Check session data.
+        self.assertTrue('modified' in self.request.session)
+        # Check session marked as modified.
+        self.assertTrue(self.request.session.modified)
+        # Check timestamp format.
+        timestamp = self.request.session['modified']
+        try:
+            datetime.strptime(timestamp, '%d/%m/%Y, %H:%M:%S')
+        except ValueError:
+            self.fail(f"Timestamp format is incorrect: {timestamp}")
