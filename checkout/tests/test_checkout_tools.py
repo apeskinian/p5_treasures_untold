a2bcf72efd340dac1ff4f1cb483444146620c8ba
Matthew Aitken <matt@itken.net>
2025-04-07 15:27:09 +0100

    Update TESTING.md: unit test checkout_tools.py

---
+++ checkout/tests/test_checkout_tools.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+
+from django.test import TestCase
+
+from ..templatetags.checkout_tools import calc_original_total
+
+
+class CheckoutToolsTest(TestCase):
+    def test_calc_original_total(self):
+        """
+        Test the calculation of an input to return 100% if given 80%.
+        """
+        original_cost = Decimal(10)
+        discounted_cost = Decimal(original_cost*Decimal(0.8))
+        self.assertEqual(calc_original_total(discounted_cost), original_cost)
